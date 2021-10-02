c0f270d17a175066a6b053d44d12d8f6af65203f
valyafrankova22 <vfrankova22@icloud.com>
2021-10-02 17:52:36 +0300

    init

---
+++ js/hw_2.js
@@ -0,0 +1,14 @@
+const num1 = prompt(`Введите первое число`);
+const num2 = prompt(`Введите второе число`);
+
+const result = num1 - num2;
+console.log(result, typeof result);
+
+const result2 = num1 + num2;
+console.log(result2, typeof result2);
+
+const result3 = num1 * num2;
+console.log(result3, typeof result3);
+
+const result4 = num1 / num2;
+console.log(result4, typeof result4);
---
+++ js/hw_3.js
@@ -0,0 +1,15 @@
+// Task #1
+let name = `Джон`;
+let admin;
+admin = name;
+
+alert(`${admin}`);
+
+// Task #2
+let planetName = (`Earth`);
+let userName = (``);
+
+
+// Task #3
+let nums = (`10` + 10);
+console.log(nums, typeof nums);
